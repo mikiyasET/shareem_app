810938254e243b4b9ab14e53e555439e63b62643
Mikiyas Lemlemu <Mikiyaslemlemu@gmail.com>
2024-03-18 19:08:11 +0300

    Core

---
+++ android/app/src/main/kotlin/com/example/shareem_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shareem_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Base());
+}
+
+class Base extends StatelessWidget {
+  const Base({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
